db9ad358c14037b0019f7a0bd0c6f3d53ad04264
xjwhhh <151250171@smail.nju.edu.cn>
2018-03-08 22:49:45 +0800

    hibernateUtil

--- src/main/java/dao/HibernateUtil.java
+++ src/main/java/dao/HibernateUtil.java
@@ -1,10 +1,8 @@
 package dao;
 
-import model.User;
+import model.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
@@ -23,6 +21,17 @@ public class HibernateUtil {
             ServiceRegistry serviceRegistry;
             config = new Configuration().configure();
             config.addAnnotatedClass(User.class);
+            config.addAnnotatedClass(Venue.class);
+            config.addAnnotatedClass(Order.class);
+            config.addAnnotatedClass(TicketManager.class);
+            config.addAnnotatedClass(Seat.class);
+            config.addAnnotatedClass(Show.class);
+            config.addAnnotatedClass(ShowSeat.class);
+            config.addAnnotatedClass(VenueApplication.class);
+            config.addAnnotatedClass(Ticket.class);
+            config.addAnnotatedClass(OrderRecord.class);
+            config.addAnnotatedClass(ShowEarning.class);
+            config.addAnnotatedClass(VenueFinance.class);
             serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
             sessionFactory = config.buildSessionFactory(serviceRegistry);
             return sessionFactory;
